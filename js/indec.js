89df10c4b1ddd6d0307dbd19b8ff6317cbea7df1
sksketer <amrit.agarwal_cs18@gla.ac.in>
2020-11-28 01:32:40 +0530

    Create indec.js

---
+++ js/indec.js
@@ -0,0 +1,6 @@
+document.getElementById('copyrightbysketeramrit').innerHTML = " &copy; copyright 2020 By sketeramrit";
+
+function footer_contactus()
+{
+    document.getElementById('span-text-a').style.cssText = 'display: block;';
+}
